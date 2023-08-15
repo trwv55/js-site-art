5b8d15e2d15743eb8b7ee29798de6fca6d6d8475
Виталий Травин <vtravin@nologostudio.ru>
2023-08-15 11:14:32 +0600

    filter.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,25 @@
-import modals from './modules/modals';
-import showMoreStyles from './modules/showMoreStyles';
-import sliders from './modules/sliders';
-import forms from './modules/forms';
-import mask from './modules/mask';
-import checkTextInputs from './modules/checkTextInputs';
-import calc from './modules/calc';
+// import filter from './modules/filter';
+// import modals from './modules/modals';
+// import showMoreStyles from './modules/showMoreStyles';
+// import sliders from './modules/sliders';
+// import forms from './modules/forms';
+// import mask from './modules/mask';
+// import checkTextInputs from './modules/checkTextInputs';
+// import calc from './modules/calc';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    showMoreStyles('.button-styles', '#styles .row');
-    modals();
-    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
-    sliders('.main-slider-item', 'vertical');
-    forms();
-    mask('[name="phone"]');
-    checkTextInputs('[name="name"]');
-    checkTextInputs('[name="message"]');
-    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    // filter();
+    // showMoreStyles('.button-styles', '#styles .row');
+    // modals();
+    // sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    // sliders('.main-slider-item', 'vertical');
+    // forms();
+    // mask('[name="phone"]');
+    // checkTextInputs('[name="name"]');
+    // checkTextInputs('[name="message"]');
+    // calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    pictureSize('.size-block');
 });
